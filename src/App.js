0407657fd15f3fa51e0a128eadea196631400b69
Nancy Sharma <nancysharma2000a@gmail.com>
2024-04-18 16:26:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/Nav';
+import Hero from './components/hero';
+function App() {
+  return (
+  <div>
+    <Navigation/>
+    <Hero/>
+  </div>
+
+  );
+}
+
+export default App;
