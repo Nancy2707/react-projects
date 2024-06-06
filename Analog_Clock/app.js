c0143c82409f62c6d72cd96e5de5252436f2b423
Nancy Sharma <nancysharma2000a@gmail.com>
2024-06-06 16:08:50 +0530

    Create app.js

---
+++ Analog_Clock/app.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from './component/Clock';
+function App() {
+  return (
+   <>
+   <div className="container">
+  <Clock/> 
+   </div>
+   </>
+  );
+}
+
+export default App;
