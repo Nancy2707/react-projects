f5ef0f74978c1eb35449920214ede2e7af7d0d23
Nancy Sharma <nancysharma2000a@gmail.com>
2024-06-07 21:23:21 +0530

    Create app.js

---
+++ Todo-app/src/app.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './Components/TodoList';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+    <TodoList/>
+    </div>
+    </>
+  );
+}
+
+export default App;
