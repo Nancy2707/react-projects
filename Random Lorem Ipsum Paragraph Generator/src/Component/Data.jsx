18760af1fbe22aeac63a18cef19dda2ef517d420
Nancy Sharma <nancysharma2000a@gmail.com>
2024-06-13 18:30:01 +0530

    Create Data.jsx

---
+++ Random Lorem Ipsum Paragraph Generator/src/Component/Data.jsx
@@ -0,0 +1,13 @@
+const text=[
+    `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae tenetur debitis ut exercitationem et? Voluptas cum quidem voluptates consectetur et.`,
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ex eos repellendus unde, asperiores quo obcaecati voluptate sapiente consectetur consequuntur quis soluta, necessitatibus facilis tenetur quam aliquam. Recusandae, autem voluptatem molestias exercitationem nisi doloremque.`,
+    `  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti suscipit rem, nemo iure amet adipisci!`,
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ipsam asperiores consectetur, sapiente quisquam impedit totam maxime autem sed sequi enim est et, ratione quo non earum adipisci provident dolore, dignissimos minus!`,
+    `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus saepe consequatur provident quaerat omnis explicabo debitis, quia unde similique consectetur dolorem ratione ea est cumque.`,
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur asperiores animi ratione minus at distinctio similique delectus atque cum, corrupti architecto, velit quibusdam eligendi amet accusamus hic non facilis maiores modi nobis voluptatem a unde perferendis molestiae. Doloremque, ab culpa. Dolore, repellendus iste!`,
+    `   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas autem nobis doloremque provident nam eum fuga animi dolorem ex voluptates modi dolore cumque saepe nemo, culpa nostrum eligendi sequi deserunt incidunt minima ab, nisi expedita corrupti.`,
+    `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam eius suscipit deserunt quas, autem reiciendis possimus excepturi enim repellat eaque dolorem.`,
+    `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad porro aliquam amet minus quaerat recusandae quidem exercitationem architecto, magni, nam reprehenderit culpa, aperiam sunt consectetur ratione cupiditate nostrum molestiae qui adipisci molestias. Necessitatibus, tempore sapiente ab quas doloribus ipsum voluptate praesentium possimus obcaecati qui. Tempore vitae hic nam architecto officiis!`,
+
+]
+export default text;
