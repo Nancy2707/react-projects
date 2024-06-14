a8c06511e51e5332cc24e92bbfe26deec7fc2415
Nancy Sharma <nancysharma2000a@gmail.com>
2024-06-14 19:12:23 +0530

    Add files via upload

---
+++ Random Color Generator/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MyApp from './Components/MyApp';
+
+function App() {
+  return (
+   <>
+   <main>
+    <h1 className='heading'>Color Code Generator</h1>
+    <MyApp/>
+   </main>
+   </>
+  );
+}
+
+export default App;
