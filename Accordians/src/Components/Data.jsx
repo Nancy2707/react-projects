c8581e8f694fdabacc1b13cebc4b8043ad6f50d5
Nancy Sharma <nancysharma2000a@gmail.com>
2024-06-14 13:54:12 +0530

    Create Data.jsx

---
+++ Accordians/src/Components/Data.jsx
@@ -0,0 +1,23 @@
+const questions=[
+    {
+        id:1,
+        title:"what do students need?",
+        info:"how are they working to achieve it"
+    },
+    {
+        id:2,
+        title:"what do people need?",
+        info:"how are they working to achieve it"
+    },
+    {
+        id:3,
+        title:"what do govt need?",
+        info:"how are they working to achieve it"
+    },
+    {
+        id:4,
+        title:"what do women need?",
+        info:"how are they working to achieve it"
+    }
+]
+export default questions
