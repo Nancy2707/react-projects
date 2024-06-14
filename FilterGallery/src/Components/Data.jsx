51a3dfb35f2549929d16899f126e5f4446c7eb00
Nancy Sharma <nancysharma2000a@gmail.com>
2024-06-14 12:57:23 +0530

    Create Data.jsx

---
+++ FilterGallery/src/Components/Data.jsx
@@ -0,0 +1,157 @@
+const item=[
+    {
+        id:1,
+        title:"food item 1",
+        category:"food",
+        price:10,
+        img:'https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZCUyMGl0ZW1zfGVufDB8fDB8fHww',
+        desc:"lorem ipsum dollar emmet"
+    },  {
+        id:2,
+        title:"food item 2",
+        category:"food",
+        price:10,
+        img:'https://images.unsplash.com/photo-1547558840-8ad6c8e662a2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZCUyMGl0ZW1zfGVufDB8fDB8fHww',
+        desc:"lorem ipsum dollar emmet"
+    },  {
+        id:3,
+        title:"food item 3",
+        category:"food",
+        price:10,
+        img:'https://images.unsplash.com/photo-1560269507-9495cdfcadca?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZCUyMGl0ZW1zfGVufDB8fDB8fHww',
+        desc:"lorem ipsum dollar emmet"
+    },  {
+        id:4,
+        title:"food item 4",
+        category:"food",
+        price:10,
+        img:'https://images.unsplash.com/photo-1464652149449-f3b8538144aa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGZvb2QlMjBpdGVtc3xlbnwwfHwwfHx8MA%3D%3D',
+        desc:"lorem ipsum dollar emmet"
+    },  {
+        id:5,
+        title:"Phone 1",
+        category:"Phones",
+        price:10,
+        img:'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8UGhvbmV8ZW58MHx8MHx8fDA%3D',
+        desc:"lorem ipsum dollar emmet"
+    },
+    {
+        id:6,
+        title:"Phone 2",
+        category:"Phones",
+        price:10,
+        img:'https://images.unsplash.com/photo-1565849904461-04a58ad377e0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fFBob25lfGVufDB8fDB8fHww',
+        desc:"lorem ipsum dollar emmet"
+    },
+    {
+        id:7,
+        title:"Phone 3",
+        category:"Phones",
+        price:10,
+        img:'https://images.unsplash.com/photo-1580910051074-3eb694886505?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fFBob25lfGVufDB8fDB8fHww',
+        desc:"lorem ipsum dollar emmet"
+    },
+    {
+        id:8,
+        title:"Phone 4",
+        category:"Phones",
+        price:10,
+        img:'https://images.unsplash.com/photo-1588058365548-9efe5acb8077?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fFBob25lfGVufDB8fDB8fHww',
+        desc:"lorem ipsum dollar emmet"
+    },
+    {
+        id:9,
+        title:"City 1",
+        category:"City",
+        price:10,
+        img:'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?q=80&w=1844&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc:"lorem ipsum dollar emmet"
+    },
+    {
+        id:10,
+        title:"City 2",
+        category:"City",
+        price:10,
+        img:'https://plus.unsplash.com/premium_photo-1682449558370-f3ef8450b1fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D',
+        desc:"lorem ipsum dollar emmet"
+    },{
+        id:11,
+        title:"City 3",
+        category:"City",
+        price:10,
+        img:'https://images.unsplash.com/photo-1519501025264-65ba15a82390?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D',
+        desc:"lorem ipsum dollar emmet"
+    },{
+        id:12,
+        title:"City 4",
+        category:"City",
+        price:10,
+        img:'https://images.unsplash.com/photo-1514924013411-cbf25faa35bb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D',
+        desc:"lorem ipsum dollar emmet"
+    },{
+        id:13,
+        title:"City 5",
+        category:"City",
+        price:10,
+        img:'https://images.unsplash.com/photo-1495954380655-01609180eda3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNpdHl8ZW58MHx8MHx8fDA%3D',
+        desc:"lorem ipsum dollar emmet"
+    },{
+        id:14,
+        title:"Shop 1",
+        category:"Shop",
+        price:10,
+        img:'https://images.unsplash.com/photo-1528698827591-e19ccd7bc23d?q=80&w=1752&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc:"lorem ipsum dollar emmet"
+    },{
+        id:15,
+        title:"Shop 2",
+        category:"Shop",
+        price:10,
+        img:'https://images.unsplash.com/photo-1570857502809-08184874388e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2hvcHxlbnwwfHwwfHx8MA%3D%3D',
+        desc:"lorem ipsum dollar emmet"
+    },{
+        id:16,
+        title:"Shop 3",
+        category:"Shop",
+        price:10,
+        img:'https://images.unsplash.com/photo-1583922606661-0822ed0bd916?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHNob3B8ZW58MHx8MHx8fDA%3D',
+        desc:"lorem ipsum dollar emmet"
+    },
+    {
+        id:17,
+        title:"Shop 4",
+        category:"Shop",
+        price:10,
+        img:'https://images.unsplash.com/photo-1581683705068-ca8f49fc7f45?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHNob3B8ZW58MHx8MHx8fDA%3D',
+        desc:"lorem ipsum dollar emmet"
+    },{
+        id:18,
+        title:"Laptop 1",
+        category:"Laptop",
+        price:10,
+        img:'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fHww',
+        desc:"lorem ipsum dollar emmet"
+    },{
+        id:19,
+        title:"Laptop 2",
+        category:"Laptop",
+        price:10,
+        img:'https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bGFwdG9wfGVufDB8fDB8fHww',
+        desc:"lorem ipsum dollar emmet"
+    },{
+        id:20,
+        title:"Laptop 3",
+        category:"Laptop",
+        price:10,
+        img:'https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGxhcHRvcHxlbnwwfHwwfHx8MA%3D%3D',
+        desc:"lorem ipsum dollar emmet"
+    },{
+        id:21,
+        title:"Laptop 4",
+        category:"Laptop",
+        price:10,
+        img:'https://images.unsplash.com/photo-1526657782461-9fe13402a841?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGxhcHRvcHxlbnwwfHwwfHx8MA%3D%3D',
+        desc:"lorem ipsum dollar emmet"
+    },
+]
+export default item;
