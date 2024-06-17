ce68860502ffbc5bb1adfea18961b1e87c5819fb
Nancy Sharma <nancysharma2000a@gmail.com>
2024-06-17 21:16:34 +0530

    Create App.js

---
+++ Add-to-Cart Functionality/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+import Shop from './Components/Shop/Shop';
+function App() {
+  return (
+    <>
+    <Shop/>
+    </>
+  );
+}
+
+export default App;
