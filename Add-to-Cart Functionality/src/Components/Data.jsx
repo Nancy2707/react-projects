701a417c814da75e4a2dee5a1eaeffa63b3e6b29
Nancy Sharma <nancysharma2000a@gmail.com>
2024-06-17 21:20:00 +0530

    Create Data.jsx

---
+++ Add-to-Cart Functionality/src/Components/Data.jsx
@@ -0,0 +1,25 @@
+const Data=[{
+    id:1,
+    title:"IDGAF",
+    price:100,
+    desc:"This is my best Selling book"
+},
+{
+    id:2,
+    title:"The Art of Moving On",
+    price:200,
+    desc:"This is my best Selling book"
+},{
+    id:3,
+    title:"Kalam's",
+    price:500,
+    desc:"This is my best Selling book"
+},
+{
+    id:4,
+    title:"A nominal house",
+    price:1000,
+    desc:"This is my best Selling book"
+}
+]
+export default Data;
