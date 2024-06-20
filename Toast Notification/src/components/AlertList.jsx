8d3aeb3a604a4fdc9d705708442d438c1d08b93d
Nancy Sharma <nancysharma2000a@gmail.com>
2024-06-20 20:40:06 +0530

    Create AlertList.jsx

---
+++ Toast Notification/src/components/AlertList.jsx
@@ -0,0 +1,16 @@
+export default[{
+    id:1,
+    type:"success",
+    title:"Thank You",
+    message:"Thank You for Shopping"
+},{
+id:2,
+type:"warning",
+title:"Warning!!",
+message:"Your Order may not have been placed"
+},{
+id:3,
+type:"error",
+title:"Error",
+message:"Unexpected Error"
+},]
